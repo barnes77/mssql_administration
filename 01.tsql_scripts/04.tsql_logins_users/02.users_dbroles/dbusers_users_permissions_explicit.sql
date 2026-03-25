89ed23eacad19b5219fd8f93f4026255679f59c6
barnes77 <ermland@protonmail.com>
2026-03-25 12:23:07 +0100

    Update dbusers_users_permissions_explicit.sql

--- 01.tsql_scripts/04.tsql_logins_users/02.users_dbroles/dbusers_users_permissions_explicit.sql
+++ 01.tsql_scripts/04.tsql_logins_users/02.users_dbroles/dbusers_users_permissions_explicit.sql
@@ -10,10 +10,11 @@ SELECT
 	OBJECT_NAME(dbper.major_id) AS [object_name]
 	,dbper.[permission_name]
 	,dbper.state_desc
-	,dbpri.[name] AS granted_by
-	,dbpri.[type_desc] AS granted_to
+	,dbpri1.[name] AS granted_by
+	,dbpri2.[name] AS granted_to
 FROM sys.database_permissions AS dbper
-JOIN sys.database_principals AS dbpri ON dbper.grantee_principal_id = dbpri.principal_id
+JOIN sys.database_principals AS dbpri1 ON dbper.grantor_principal_id = dbpri1.principal_id
+JOIN sys.database_principals AS dbpri2 ON dbper.grantee_principal_id = dbpri2.principal_id
 WHERE 1=1
 	AND OBJECT_NAME(dbper.major_id) IS NOT NULL
 	AND dbpri.[name] LIKE '%principalname%' --Comment out this condition to list all permissions on database level
